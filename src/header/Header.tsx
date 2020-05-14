c448f6678388fd2835fe12a86ea74355f04f1659
liorbenita <liorbenita@gmail.com>
2020-05-14 03:35:35 +0300

    header v1 + homepage v1
    layout, colors , logo

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Header.less'
+
 
 class Header extends React.Component{
 
     render(){
         return (
             <div className="header">
-                <div className="header-logo">logo</div>
+                <div className="header-logo">
+                    <img className="logo" src={require('../assets/logo.png')} alt="" />
+                </div>
                 <div className="header-search"><input type="text"/></div>
                 <div className="header-account">Account</div>
                 <div className="header-setting">cogwhell</div>
--- src/homePage/HomePage.tsx
+++ src/homePage/HomePage.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import './HomePage.less'
 
 class HomePage extends React.Component { 
 
     render(){
         return (
-            <div>
-            coming soon :)  GeekBoards 
+            <div className="homepage-container">
+                <div className="homepage-sidebar-right">
+
+                </div>
+                <div className="homepage-mainthread">
+                    coming soon :)  GeekBoards 
+                </div>
+                <div className="homepage-sidebar-left">
+
+                </div>
+            
           </div>
         )
     }
